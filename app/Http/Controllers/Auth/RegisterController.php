d875a487a01b4ec178f1da3f0f58b0d842ee9690
Dinh Ky <dinhky0204@gmail.com>
2017-07-13 15:07:57 +0700

    login, sigup, reset password

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Models\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
@@ -66,6 +69,39 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'active' => false,
+            'point' => 0,
+            'avatar' => 'default',
         ]);
+    }
+    protected function register(Request $request) {
+        $validator = $this->validator($request->all());
+        if($validator->fails()) {
+            return redirect()->back()->withInput()->with('register_error', $validator->messages());
+        }
+        else
+        {
+            User::where('email', $request->input('email'))->where('active', 0)->delete();
+            $data = $this->create($request->all())->toArray();
+            $minutes = 1;
+            $random_token = str_random(30);
+            $data['token'] = $random_token;
+            Cache::add($random_token, $data['email'], $minutes);
+            Mail::send('mails.confirmation', $data, function ($message) use($data) {
+                $message->to($data['email']);
+                $message->subject('Registration Confirmation');
+            });
+            return redirect(route('login'))->with('register_status', '確認メールが送信されました。 メールを確認してください。');
+        }
+    }
+    public function confirmation($token) {
+        $value = Cache::get($token);
+        if (!is_null($value)) {
+            $user = User::where('email', $value)->first();
+            $user->active = 1;
+            $user->save();
+            return redirect(route('home'))->with('confirm_status', 'Your activation is completed.');
+        }
+        return redirect(route('login'))->with('confirm_status', 'Something went wrong.');
     }
 }
